33767be83665bd4ff449185ec19e1888a17ec501
Andy Hsu <i@nn.ci>
2026-03-27 22:13:36 +0800

    Initial Commit

---
+++ ContextKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ContextKit
+//
+//  Created by xhofe on 2026/3/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ContextKit/ContextKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextKitApp.swift
+//  ContextKit
+//
+//  Created by xhofe on 2026/3/27.
+//
+
+import SwiftUI
+
+@main
+struct ContextKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
